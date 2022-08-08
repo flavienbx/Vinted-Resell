68c8d74bff3079ea0ef18db99b8807fe2841af8a
Flavien <46301747+flavienbx@users.noreply.github.com>
2022-08-09 00:56:00 +0200

    Add files via upload

---
+++ src/install.bat
@@ -0,0 +1,34 @@
+pip install glob
+pause
+pip install lib2to3.pgen2
+pause
+pip install pickle
+pause
+pip install selenium
+pause
+pip install selenium.webdriver.common.by
+pause
+pip install webdriver_manager.chrome
+pause
+pip install ChromeDriverManager
+pause
+pip install discord_webhook
+pause
+pip install DiscordWebhook
+pause
+pip install os
+pause
+pip install chromedriver_binary
+pause
+pip install requests
+pause
+pip install time
+pause
+pip install pyfiglet
+pause
+pip install os
+pause
+pip install threading
+pause
+pip install json
+pause
